852a99efd0541a19ea135a2e8d770f5ff80f7eaf
nik <nik_rb@yahoo.com>
2016-08-07 23:28:55 +0100

    little state example on timer

--- src/js/components/Layout.js
+++ src/js/components/Layout.js
@@ -4,9 +4,18 @@ import Header from './Header';
 import Footer from './Footer';
 
 export default class Layout extends React.Component {
+  constructor(){
+    super();
+    // use state only if this component affected
+    this.state = { name:"nik"};
+  }
   render(){
+    setTimeout( () => {
+      this.setState( { name : "bob"});
+    }, 1000);
     return (
       <div>
+        {this.state.name}
         <Header />
         <Footer />
       </div>
