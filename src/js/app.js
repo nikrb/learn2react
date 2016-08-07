6e37b98ddfb45352908627edc83c9a7530169180
nik <nik_rb@yahoo.com>
2016-08-08 00:38:56 +0100

    add bootstrap import

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import Layout from './components/Layout';
+import Bootstrap from 'bootstrap-without-jquery';
+
+import Layout from './pages/Layout';
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
