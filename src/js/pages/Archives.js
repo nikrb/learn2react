ab7a5afd30f19a5ba2d233591b43b9dcbc92824c
nik <nik_rb@yahoo.com>
2016-08-08 17:13:52 +0100

    add some articles to archive page

--- src/js/pages/Archives.js
+++ src/js/pages/Archives.js
@@ -1,15 +1,29 @@
 import React from 'react';
 
+import Article from '../components/Article';
+
 export default class Archives extends React.Component {
   render(){
     const { query} = this.props.location;
     const { params} = this.props;
-    const { article} = params;
-    const { date, filter} = query;
+    let { article} = params;
+    let { date, filter} = query;
+
+    if( !article) article = "All";
+    if( !date) date = "All";
+    if( !filter) filter = "All";
+
+    const articles = [
+      "Some Article",
+      "Another Article",
+      "Yet Another Article",
+      "Article For The Road"
+    ].map( (title, i) => <Article key={i} title={title}/> );
     return (
       <div>
         <h1>Archives ({article})</h1>
         <h4>date: {date}. filter: {filter}</h4>
+        <div className="row" >{articles}</div>
       </div>
     );
   }
