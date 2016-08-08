f0707503f95695bebedc4747d54cd942a5d7cd8a
nik <nik_rb@yahoo.com>
2016-08-08 10:29:10 +0100

    rework layout page

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,24 +1,27 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import Footer from '../components/layout/Footer.js';
+import Nav from '../components/layout/Nav.js';
+
 export default class Layout extends React.Component {
-  // FIXME: dynamic link - this doesn't actually work
-  navigate(){
-    // deprecated this.props.history.push( null, "/");
-    // cannot read property pushState of undefined
-    // this.context.router.pushState( null, "/");
-  }
   render(){
-    // FIXME: again deprecated
-    const { history} = this.props;
-    console.log( history.isActive( "archives"));
+    const { location } = this.props;
+    const containerStyle = {
+      marginTop : "60px"
+    };
     return (
       <div>
-        <h1>KillerNews.net</h1>
-        {this.props.children}
-        <Link to="archives" activeClassName="test">Archives</Link>
-        <Link to="settings"><button className="btn btn-success">Settings</button></Link>
-        <button onClick={this.navigate.bind(this)}>Featured</button>
+        <Nav location={location} />
+
+        <div className="container" style={containerStyle} >
+          <div className="row">
+            <div className="col-lg-12">
+              <h1>KillerNews.net</h1>
+              {this.props.children}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
