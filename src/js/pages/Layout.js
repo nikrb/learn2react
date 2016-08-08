f426086efd7f80831be4c6d62411e49cfe80e6f7
nik <nik_rb@yahoo.com>
2016-08-08 01:09:49 +0100

    add new pages and router - note history.pushState is deprecated

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router';
 
 export default class Layout extends React.Component {
+  // FIXME: dynamic link - this doesn't actually work
+  navigate(){
+    // deprecated this.props.history.push( null, "/");
+    // cannot read property pushState of undefined
+    // this.context.router.pushState( null, "/");
+  }
   render(){
     return (
-      <h1>KillerNews.net</h1>
+      <div>
+        <h1>KillerNews.net</h1>
+        {this.props.children}
+        <Link to="archives" className="btn btn-danger">Archives</Link>
+        <Link to="settings"><button className="btn btn-success">Settings</button></Link>
+        <button onClick={this.navigate.bind(this)}>Featured</button>
+      </div>
     );
   }
 }
